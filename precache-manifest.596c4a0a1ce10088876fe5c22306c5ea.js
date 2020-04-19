93b314c3ac8271332693b826a26667933146ae0e
Lenasim <radical.rin@gmail.com>
2020-04-19 18:36:11 +0200

    Updates

---
+++ precache-manifest.596c4a0a1ce10088876fe5c22306c5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544f19bef892a21c481a39362a1b3cca",
+    "url": "/react-12-challenge/index.html"
+  },
+  {
+    "revision": "935211c9ea4fa4cbad6a",
+    "url": "/react-12-challenge/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "e20b232851c8f4a7a77a",
+    "url": "/react-12-challenge/static/js/2.38e0c761.chunk.js"
+  },
+  {
+    "revision": "935211c9ea4fa4cbad6a",
+    "url": "/react-12-challenge/static/js/main.623bd691.chunk.js"
+  },
+  {
+    "revision": "909d4b0ae5d68cd074e6",
+    "url": "/react-12-challenge/static/js/runtime-main.542737b9.js"
+  }
+]);
